811fcf8462a76d75dd63c898254001e015d39a17
Leah Chercham <leahchercham@hotmail.com>
2019-10-15 16:46:56 -0500

    input fields for user

--- src/user/landing/create-post/CreatePost.js
+++ src/user/landing/create-post/CreatePost.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class CreatePost extends Component {
+    constructor(){
+        super()
+        this.state = {
+            post = {
+                title: "",
+                content:"",
+                //category:"",
+                points:0,
+                date:"",
+                address:"",
+            },
+            usersPhone: null, // login with number // Pass it from Login
+        }
+    }
+
+    handleInputChange = (event) => {
+        let name = event.target.name
+        let value = event.target.value
+        let post = {...this.state.post}
+        post[name] = value
+    }
+
+    createPost = () => {
+        let usersPhone = this.state.usersPhone
+        let post = {...this.state.post}
+        let date = new Date()
+        post.date = date
+        // send two objects: req.body.post and req.body.phone
+        let data = {post : post, usersPhone : usersPhone}
+        axios.post("http://localhost:4000/data/post", data )
+    }
+
     render() {
         return (
             <div>
-                <input type="text" name="postTitle" placeholder="Title"/>
-                <input type="text" name="postText" placeholder="Text"/>
-                <input type="text" name="postLocation" placeholder="Location"/>
-                <button>Submit</button>
+                <input type="text" name="title" placeholder="Title" onChange={this.handleInputChange}/>
+                <input type="text" name="content" placeholder="Text" onChange={this.handleInputChange}/>
+                <input type="text" name="address" placeholder="Adress" onChange={this.handleInputChange}/>
+                <button onClick={this.createPost}>Submit</button>
                 {/* Pop up please review your post */}
             </div>
         );
