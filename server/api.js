fec8e374ecc012d4ad0f7af4f2995e9e32fc7f70
unknown <shmtomer@gmail.com>
2019-10-15 19:30:30 -1000

    populations

--- server/api.js
+++ server/api.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const Post = require('./schemas/Post')
 const User = require('./schemas/User')
-const data = require('./data')
 
 
 
@@ -11,22 +10,26 @@ router.get('/data/posts', async (req, res) => {
     res.send(posts)
 })
 
+const updateUserPosts=(usersPhone,post)=>{
+    return User.findOneAndUpdate({
+        "phone": usersPhone
+    }, {
+        "$push": {
+            "posts": post
+        }
+    }, {
+        "new": true
+    })
+}
+
 router.post('/data/post', async (req, res) => {
-    let post = new Post(req.body)
-    await post.save()
-    let user = await User.findById(post.user)
-    user.posts.push(post)
-    await user.save()
-    // await User.findByIdAndUpdate(
-    //     post.user
-    // , {
-    //     "$push": {
-    //         "posts": post
-    //     }
-    // }, {
-    //     "new": true
-    // })
-    // console.log(user)
-    res.send(user)
+    let post = new Post(req.body.post)
+    post.user= await User.findOne({"phone":req.body.usersPhone})
+    post.save((err,doc)=>console.log(doc)) 
+    
+    updateUserPosts(req.body.usersPhone,post)
+    .then((doc)=>res.send(doc))
+    
 })
-module.exports = router
+
+module.exports= router
