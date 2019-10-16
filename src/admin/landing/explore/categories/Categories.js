bfc872ae4ff7054dea4948eb62cef66f778e305c
Axel <mophead38@gmail.com>
2019-10-16 14:53:21 -0500

    ...

--- src/admin/landing/explore/categories/Categories.js
+++ src/admin/landing/explore/categories/Categories.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import Category from './Category';
+import React, { Component } from "react";
+import Category from "./Category";
 class Categories extends Component {
-    constructor() {
-        super()
-        this.state = {
-            categories : [
-                "Violence",
-                "Public Disturbance",
-                'Tresspassing',
-                "Public Space",
-                "Utilities",
-                "General",
-                "Noise",
-                "Disorderly Conduct"
-            ]
-        }
-    }
-    render() {
-        return (<div>
-            <div></div>
-            {/* {this.state.categories.map(c => {
+  constructor() {
+    super();
+    this.state = {
+      categories: [
+        "Violence",
+        "Public Disturbance",
+        "Tresspassing",
+        "Public Space",
+        "Utilities",
+        "General",
+        "Noise",
+        "Disorderly Conduct"
+      ]
+    };
+  }
+  render() {
+    return (
+      <div>
+        <div></div>
+        {this.state.categories.map(c => {
+                return <div>{c}</div>
+            })}
+        {/* {this.state.categories.map(c => {
                 return <Category category={c} />
             })} */}
-        </div>)
-    }
+      </div>
+    );
+  }
 }
 export default Categories;
