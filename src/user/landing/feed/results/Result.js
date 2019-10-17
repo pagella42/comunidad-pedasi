d5d0030dbff33caa40d243a097c0b2b870330c48
pagella42 <pagella42@gmail.com>
2019-10-17 10:19:21 -0500

    comm

--- src/user/landing/feed/results/Result.js
+++ src/user/landing/feed/results/Result.js
@@ -1,5 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 class Result extends Component {
+    constructor() {
+        super()
+        this.state = {
+          comment: ''
+        }
+    }
+    update = async (event) => {
+        await this.setState({
+            [event.target.name]: event.target.value,
+        })
+    }
+    async comment(){
+       await axios.post('', {comment: this.state.comment})
+       //clean comment input \/
+       this.setState({comment: ""})
+    }
     render() {
     let post = this.props.post
         return (<div>
@@ -8,9 +25,16 @@ class Result extends Component {
             <div>{post.points}</div>
             <div>{post.content}</div>
             <div>{post.address}</div>
-            <div>CATEGORY</div>
+            <div>{post.category}</div>
             <div>IMAGES</div>
             <div>TOWN RESPONSE</div>
+
+            {/* post comment  \/ */}
+            <input type="text" name="comment" placeholder="Comment something" value={this.state.comment} onChange={this.update}/>
+            <button onClick={this.comment}>Send comment</button>
+
+            {/* render comments \/ */}
+            <div>{post.comments.map(c=> <div>{/* GET USERNAME AND CONTENT */}</div>)}</div>
         </div>)
     }
 }
