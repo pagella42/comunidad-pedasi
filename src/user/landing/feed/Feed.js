93e7ef59cec5bde6ebf4c1ee31697b3ce33dd0bb
Axel <mophead38@gmail.com>
2019-10-21 12:20:48 -0500

    added begginings of filter component

--- src/user/landing/feed/Feed.js
+++ src/user/landing/feed/Feed.js
@@ -2,41 +2,34 @@ import React, { Component } from 'react';
 import Results from './results/Results'
 import axios from 'axios';
 import CreatePost from '../create-post/CreatePost';
+import Filter from './filter/Filter';
 class Feed extends Component {
     constructor(){
         super()
         this.state={
             posts: [],
-            filter: 'Public Disturbance',
+      
         }
     }
 
-    getAllPosts = async () => {
+    getPosts = async (parameters) => {
         // create a loader here
         let response = await axios.get("http://localhost:4000/data/posts")
         let posts = response.data
-        posts.sort((a, b) => (a.date > b.date) ? -1 : 1)
-        this.setState({posts : posts})  
-    }
 
-    getFilterdPosts = async () => {
-        let response = await axios.get(`http://localhost:4000/data/posts/category/${this.state.filter}`)
-        let posts = response.data
-        posts.sort((a, b) => (a.date > b.date) ? -1 : 1)
         this.setState({posts : posts})  
     }
 
     async componentDidMount(){
-        this.state.filter? await this.getFilterdPosts() :
-        await this.getAllPosts()
+        await this.getPosts()
     }
 
 
     render() {
         return (
             <div>
-            {/* call filter comp */}
-            <CreatePost phone={this.props.phone} getAllPosts={this.getAllPosts}/>
+            <CreatePost phone={this.props.phone} getPosts={this.getPosts}/>
+            <Filter getPosts={this.getPosts}/>
             <Results posts={this.state.posts} phone={this.props.phone}/>
         </div>
         )
---
+++ src/user/landing/feed/filter/Filter.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+class Filter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sort: "dh",
+      filter: {
+        sort: {
+          by: "date",
+          order: 1
+        },
+        category: "",
+        status: "",
+        language: ""
+      }
+    };
+  }
+  filter = async () => {
+    let filter = this.state.filter;
+    switch (this.state.sort) {
+      case "dh":
+        filter.sort.by = "date";
+        filter.sort.order = 1;
+        
+        break;
+      case "dl":
+        filter.sort.by = "date";
+        filter.sort.order = -1;
+        
+        break;
+      default:
+        console.log("nothing");
+    }
+
+
+
+    this.setState({filter})
+    
+
+    this.props.getPosts(this.state.filter);
+    console.log(this.state);
+  };
+
+  updateSort = async event => {
+    await this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  update = async event => {
+      let filter = this.state.filter
+      filter[event.target.name] = event.target.value
+
+    await this.setState({
+        filter
+    });
+  };
+  render() {
+    return (
+      <div>
+        <select
+          name="sort"
+          onChange={this.updateSort}
+          value={this.state.sort}
+          id=""
+        >
+          <option value="dh">date high to low</option>
+          <option value="dl">date low to hight</option>
+          <option value="ph">Points hight low</option>
+          <option value="pl">Points low to high</option>
+        </select>
+
+         <select
+          name="category"
+          onChange={this.update}
+          value={this.state.filter.category}
+          id=""
+        >
+          <option value="Disorderly Conduct">Disorderly Conduct</option>
+          <option value="General">General</option>
+          <option value="Noise">Noise</option>
+          <option value="Public Disturbance"></option>
+          <option value="Tresspassing"></option>
+          <option value="Utilities">Utilities</option>
+          <option value="Violence">Violence</option>
+        </select>
+
+        <select
+          name="status"
+          onChange={this.update}
+          value={this.state.filter.status}
+          id=""
+        >
+          <option value="pending aproval">pending aproval</option>
+          <option value="in discussion">In Discussion</option>
+          <option value="Resovled">Resovled</option>
+        </select>
+
+        <select
+          name="language"
+          onChange={this.update}
+          value={this.state.filter.language}
+          id=""
+        >
+          <option value="Espanol">Espanol</option>
+          <option value="en">English</option>
+
+       
+        </select>
+        <button onClick={this.filter}>Send</button>
+      </div>
+    );
+  }
+}
+export default Filter;
