96b347b1688474b57716545a6c8f2b75dddb06d4
Leah Chercham <leahchercham@hotmail.com>
2019-10-21 15:16:43 -0500

    getting array of users

--- src/admin/landing/search/filter/Filter.js
+++ src/admin/landing/search/filter/Filter.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 import KeywordSearch from './KeywordSearch';
+import UserSearch from './UserSearch';
 
 class Filter extends Component {
     render() {
         return (
             <div>
                 <KeywordSearch saveFoundPosts={this.props.saveFoundPosts}/>
+                <UserSearch />
             </div>
         );
     }
---
+++ src/admin/landing/search/filter/UserSearch.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+
+
+class UserSearch extends Component {
+    constructor(){
+        super()
+        this.state = {
+            posts: [],
+            users: [],
+        }
+    }
+    getAllPosts = async () => {
+        let response = await Axios.get('http://localhost:4000/data/posts')
+        this.setState({ posts: response.data })
+    }
+    getAllUsers = async () => {
+        debugger
+        let response = await Axios.get('http://localhost:4000/data/users')
+        this.setState({ users: response.data})
+    }
+    componentDidMount(){
+        this.getAllUsers()
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default UserSearch;
