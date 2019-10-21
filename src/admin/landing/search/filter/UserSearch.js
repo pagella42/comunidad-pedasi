078c960277949223a11c2464c8e0b2f3b3b6f285
Leah Chercham <leahchercham@hotmail.com>
2019-10-21 15:30:31 -0500

    search by name works

--- src/admin/landing/search/filter/UserSearch.js
+++ src/admin/landing/search/filter/UserSearch.js
@@ -3,28 +3,53 @@ import Axios from 'axios';
 
 
 class UserSearch extends Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
             posts: [],
             users: [],
+            user: { name: "", phone: "" }, 
+            postFromUser: [],
         }
     }
+    handleChange = (e) => {
+        let name = e.target.value
+        let phone = this.state.users.filter(user => user.name === name).map(c => c.phone)[0]
+        let user = {...this.state.user}
+        user.name = name
+        user.phone = phone
+        this.setState({user: user})
+    }
+
+    search = () => {
+        let posts = this.state.posts
+        let postFromUser = posts.filter(post => post.user.phone === this.state.user.phone)
+        this.setState({postFromUser : postFromUser})
+    }
+
     getAllPosts = async () => {
         let response = await Axios.get('http://localhost:4000/data/posts')
         this.setState({ posts: response.data })
     }
     getAllUsers = async () => {
         let response = await Axios.get('http://localhost:4000/data/users')
-        this.setState({ users: response.data})
+        this.setState({ users: response.data })
     }
-    componentDidMount(){
+    componentDidMount() {
         this.getAllUsers()
+        this.getAllPosts()
     }
     render() {
         return (
             <div>
-
+                <div>
+                    User:
+                </div>
+                <input type="text" placeholder="User name" list="data" onChange={this.handleChange} />
+                <datalist id="data">
+                    {this.state.users.map(user => <option value={user.name} key={user._id} />)}
+                </datalist>
+                <button onClick={this.search}>Search for posts from user</button>
             </div>
         );
     }
