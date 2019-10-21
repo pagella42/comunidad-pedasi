dfb2133842dd2ef8e1bc69ba92276e38c4e2f3fb
Axel <mophead38@gmail.com>
2019-10-21 13:31:25 -0500

    filter object

--- src/user/landing/feed/filter/Filter.js
+++ src/user/landing/feed/filter/Filter.js
@@ -9,9 +9,9 @@ class Filter extends Component {
           by: "date",
           order: 1
         },
-        category: "",
-        status: "",
-        language: ""
+        category: "All",
+        status: "All",
+        language: "All"
       }
     };
   }
@@ -21,24 +21,31 @@ class Filter extends Component {
       case "dh":
         filter.sort.by = "date";
         filter.sort.order = 1;
-        
+
         break;
       case "dl":
         filter.sort.by = "date";
         filter.sort.order = -1;
-        
+
+        break;
+      case "ph":
+        filter.sort.by = "points";
+        filter.sort.order = 1;
+
+        break;
+      case "pl":
+        filter.sort.by = "points";
+        filter.sort.order = -1;
+
         break;
       default:
         console.log("nothing");
     }
 
-
-
-    this.setState({filter})
-    
+    this.setState({ filter });
 
     this.props.getPosts(this.state.filter);
-    console.log(this.state);
+    console.log(this.state.filter);
   };
 
   updateSort = async event => {
@@ -48,16 +55,17 @@ class Filter extends Component {
   };
 
   update = async event => {
-      let filter = this.state.filter
-      filter[event.target.name] = event.target.value
+    let filter = this.state.filter;
+    filter[event.target.name] = event.target.value;
 
     await this.setState({
-        filter
+      filter
     });
   };
   render() {
     return (
       <div>
+          <label htmlFor="">sort by</label>
         <select
           name="sort"
           onChange={this.updateSort}
@@ -69,13 +77,14 @@ class Filter extends Component {
           <option value="ph">Points hight low</option>
           <option value="pl">Points low to high</option>
         </select>
-
-         <select
+        <label htmlFor="">category</label>
+        <select
           name="category"
           onChange={this.update}
           value={this.state.filter.category}
           id=""
         >
+          <option value="All">All</option>
           <option value="Disorderly Conduct">Disorderly Conduct</option>
           <option value="General">General</option>
           <option value="Noise">Noise</option>
@@ -85,27 +94,29 @@ class Filter extends Component {
           <option value="Violence">Violence</option>
         </select>
 
+        <label htmlFor="">status</label>
         <select
           name="status"
           onChange={this.update}
           value={this.state.filter.status}
           id=""
         >
+          <option value="All">All</option>
           <option value="pending aproval">pending aproval</option>
           <option value="in discussion">In Discussion</option>
           <option value="Resovled">Resovled</option>
         </select>
 
+        <label htmlFor="">language</label>
         <select
           name="language"
           onChange={this.update}
           value={this.state.filter.language}
           id=""
         >
+          <option value="All">All</option>
           <option value="Espanol">Espanol</option>
           <option value="en">English</option>
-
-       
         </select>
         <button onClick={this.filter}>Send</button>
       </div>
