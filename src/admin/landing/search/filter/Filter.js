f92c8d61fa3bed92b28bffaad147bac8ddc1cc2a
Leah Chercham <leahchercham@hotmail.com>
2019-10-21 15:08:23 -0500

    files restructuration

--- src/admin/landing/search/filter/Filter.js
+++ src/admin/landing/search/filter/Filter.js
@@ -1,102 +1,12 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
+import KeywordSearch from './KeywordSearch';
 
 class Filter extends Component {
-    constructor() {
-        super()
-        this.state = {
-            keyword: "",
-            found: [],
-            posts: [],
-            keywordSearch: [
-                { name: "title", checked: false },
-                { name: "content", checked: false },
-                { name: "comments", checked: false },
-                { name: "responses", checked: false },
-
-            ]
-        }
-    }
-
-    handleChange = (e) => {
-        let name = e.target.name
-        if (name === "search-keyword") {
-            let choice = e.target.id
-            let checked = e.target.checked
-
-            let keywordSearch = [...this.state.keywordSearch]
-            let keywordIndex = keywordSearch.findIndex(keyword => keyword.name === choice)
-            keywordSearch[keywordIndex].checked = checked
-            this.setState({ keywordSearch: keywordSearch })
-
-        } else {
-            let value = e.target.value
-            this.setState({ [name]: value })
-        }
-    }
-
-    handleSearch = () => {
-        let keywordSearch = [...this.state.keywordSearch]
-        let checkedCheckboxes = keywordSearch.filter(keyword => keyword.checked === true)
-        let options = checkedCheckboxes
-        this.search(options, this.state.keyword)
-    }
-
-    getAllPosts = async () => {
-        let response = await Axios.get('http://localhost:4000/data/posts')
-        this.setState({ posts: response.data })
-    }
-
-    search = (options, text) => {
-        let searchArray = text.toLowerCase()
-        let filteredPosts = []
-        let nothing
-        this.state.posts.map(post => {
-            options.forEach(option => {
-                if (option.name === "comments" || option.name === "responses") {
-                    post[option.name].forEach(name => {
-                        let value = name.content.toLowerCase()
-                        if (value.includes(searchArray)) {
-                            filteredPosts.includes(post) ? nothing = null : filteredPosts.push(post)
-                        } else { return undefined }
-                    })
-                } else {
-                    let value = post[option.name].toLowerCase()
-                    if (value.includes(searchArray)) {
-                        filteredPosts.includes(post) ? nothing = null : filteredPosts.push(post)
-                    } else { return undefined }
-                }
-            })
-        })
-        this.setState({ found: filteredPosts })
-        this.props.saveFoundPosts(filteredPosts)
-    }
-
-    componentDidMount() {
-        this.getAllPosts()
-    }
-
     render() {
         return (
             <div>
-                <input type="text" name="keyword" placeholder="Search by Keyword" onChange={this.handleChange} />
-                <div>
-                    <input type="checkbox" name="search-keyword" id="title" onClick={this.handleChange} />
-                    <label htmlFor="title-checkbox">title</label>
-                </div>
-                <div>
-                    <input type="checkbox" name="search-keyword" id="content" onClick={this.handleChange} />
-                    <label htmlFor="content-checkbox">content</label>
-                </div>
-                <div>
-                    <input type="checkbox" name="search-keyword" id="comments" onClick={this.handleChange} />
-                    <label htmlFor="comments-checkbox">comments</label>
-                </div>
-                <div>
-                    <input type="checkbox" name="search-keyword" id="responses" onClick={this.handleChange} />
-                    <label htmlFor="responses-checkbox">responses</label>
-                </div>
-                <button onClick={this.handleSearch}>Search</button>
+                <KeywordSearch saveFoundPosts={this.props.saveFoundPosts}/>
             </div>
         );
     }
