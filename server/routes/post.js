09d3c0ce90dee2bf20178d8b3efa3db6007876db
jona <jona@elevation.ac>
2019-10-22 03:04:05 +0300

    fix By bug

--- server/routes/post.js
+++ server/routes/post.js
@@ -42,8 +42,15 @@ router.post('/data/post/:usersPhone', async (req, res) => {
 
 })
 
-router.post('/data/posts',(req,res)=>{
+router.post('/data/posts', async (req,res)=>{
     let{sort,category,status,language}=req.body
+    
+    // this is a hack. Should find a better way to determine whether there are filter params or not
+    if(!req.body.sort){
+        const allPosts = await Post.find({})
+        return res.send(allPosts)
+    }
+
     Post.find(makeFilterObject(category,status,language))
     .populate(`user comments responses`)
     .sort({[sort.by]:sort.order})
