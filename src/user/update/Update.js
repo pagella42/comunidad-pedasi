94aa1e548ac5f239c46eac2ce0ae4c86262525e1
Leah Chercham <leahchercham@hotmail.com>
2019-10-26 13:04:59 -0500

    some materialize for my info

--- src/user/update/Update.js
+++ src/user/update/Update.js
@@ -1,23 +1,30 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { withTranslation } from 'react-i18next';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import { TextField, Card } from '@material-ui/core';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from "@material-ui/styles";
+
 
 class Update extends Component {
   constructor() {
     super();
     this.state = {
       login: JSON.parse(localStorage.userLogin),
-      details: ["name", "address","ID", "email"],
+      details: ["name", "address", "ID", "email"],
       inputs: {
         name: "",
         address: "",
-        phone:"",
-        ID:"",
-        email:""
+        phone: "",
+        ID: "",
+        email: ""
       },
-      pin:null
+      pin: null
     };
   }
+  
   update = async () => {
     let data = this.state.inputs
 
@@ -31,10 +38,10 @@ class Update extends Component {
   handleInput = e => {
     let inputs = { ...this.state.inputs };
     inputs[e.target.name] = e.target.value;
-    this.setState({ inputs },()=>{
-      this.state.inputs.phone==''?
-      document.getElementById('edit-verify').disabled=false:
-      document.getElementById('edit-verify').disabled=true
+    this.setState({ inputs }, () => {
+      this.state.inputs.phone == '' ?
+        document.getElementById('edit-verify').disabled = false :
+        document.getElementById('edit-verify').disabled = true
       console.log(document.getElementById('edit-verify').disabled)
     });
   };
@@ -68,64 +75,81 @@ class Update extends Component {
 
   };
   render() {
-    const {t,i18n} = this.props
+    const { t, i18n } = this.props
     return (
-      <div>
-          <div>
-        {this.state.userData ? 
-          this.state.details.map(detail => (
+      <Container maxWidth="sm">
+        <Card>
+          <CardContent>
             <div>
-                <label htmlFor="">{t(detail)}</label>
-                <input
-                  type="text"
-                  onChange={this.handleInput}
-                  value={this.state.inputs[detail]}
-                  name={detail}
-                  placeholder={t(this.state.userData[detail])}
-                />
+              {this.state.userData ?
+                this.state.details.map(detail => (
+                  <div
+                  >
+                    <TextField
+                      id="outlined-name"
+                      margin="normal"
+                      variant="outlined"
+                      label={t(detail)}
+                      type="text"
+                      onChange={this.handleInput}
+                      value={this.state.inputs[detail]}
+                      name={detail}
+                      placeholder={t(this.state.userData[detail])}
+                    />
+                  </div>
+                ))
+                : null}
             </div>
-          ))
-          : null}
-          </div>
-          <div>
-            <span>
-              {t("Phone Number")}
-              <input
-                required
-                value={this.state.inputs.phone}
-                onChange={this.handleInput}
-                name="phone"
-              ></input>
-              *
-            </span>
-            <input
-              type="button"
-              value="Validate Number"
-              onClick={this.sendCode}
-            />
-          </div>
+            <div>
+              <div>
+                <TextField
+                  required
+                  id="outlined-name"
+                  margin="normal"
+                  variant="outlined"
+                  label={t("Phone Number")}
+                  value={this.state.inputs.phone}
+                  onChange={this.handleInput}
+                  name="phone"
+                />
 
-          <div>
-            <span>
-              {t("Verify Pin")}
-              <input
-                required
-                value={this.state.pin}
-                onChange={this.handlePin}
-                name="pin"
-              ></input>
-              *
-            </span>
-            <input
-              type="button"
-              value="Validate"
-              onClick={this.verifycode}
-            />
-          </div>
-          <button id='edit-verify' onClick={this.update}>{t("Update")}</button>
+              </div>
+              <Button
+                // type="button"
+                value="Validate Number"
+                variant="contained" color="secondary"
+                onClick={this.sendCode}
+              >Validate Number</Button>
+            </div>
 
-      </div>
+            <div>
+              <div>
+
+                <TextField
+                  required
+                  id="outlined-name"
+                  margin="normal"
+                  variant="outlined"
+                  label={t("Verify Pin")}
+                  value={this.state.pin}
+                  onChange={this.handlePin}
+                  name="pin"
+
+                />
+
+              </div>
+              <Button
+                // type="button"
+                value="Validate"
+                variant="contained" color="secondary"
+                onClick={this.verifycode}
+              >Validate</Button>
+            </div>
+            <Button variant="contained" color="primary" id='edit-verify' onClick={this.update}>{t("Update")}</Button>
+          </CardContent>
+        </Card>
+      </Container>
     );
   }
 }
-export default withTranslation('translation') (Update);
+export default withTranslation('translation')(Update);
