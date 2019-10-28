c62931f42bd45ae383bae6989354232d348f04f7
JovanDyaz <jvaonam@me.com>
2019-10-28 03:34:23 -0500

    addCategory

--- src/admin/manage/Manage.js
+++ src/admin/manage/Manage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Header from '../Header'
+import axios from 'axios'
 import { withTranslation } from 'react-i18next';
 
 import Button from '@material-ui/core/Button';
@@ -9,21 +10,47 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import Consts from '../../Consts'
+
+const CREATE_ROUTE = Consts.CREATE_ROUTE
 
 class Manage extends Component {
     constructor() {
         super()
         this.state = {
-            categories: ["Other"],
-            statusPost: ["Pending"]
+            categories: [""],
+            newCategory: "",
+            status: ["Pending"]
         }
     }
 
     update = event => this.setState({ [event.target.name]: event.target.value })
 
-    addStatus = () => {
-        
+    getCategories = async () => {
+        let response = await axios.get(CREATE_ROUTE("data/categories"))
+        let categories = []
+        if (response.data[0] === undefined) {
+            categories = ["Other"]
+        } else {
+            response.data.forEach(category => categories.push(category.name))
+        }
+        this.setState({ categories }, () => console.log(this.state.categories))
+    }
+
+    addCategory = async () => {
+        console.log(this.state.newCategory);
+        if (window.confirm(`Confirm New Category: ${this.state.newCategory}`)) {
+            await axios.post(CREATE_ROUTE(`data/category/${this.state.newCategory}`))
+            this.getCategories()
+        }
+        else alert("Not sent")
+        this.setState({ newCategory: "" })
+    }
+
+    componentDidMount = async () => {
+        this.getCategories()
     }
 
     render() {
@@ -32,27 +59,28 @@ class Manage extends Component {
             <div>
                 <Header />
 
-                <div id="fieldcont">
+                <Card className='logincontainer' style={{ maxWidth: 500 }}>
+                    <CardContent>
+                        <div id="fieldcont">
+                            <FormControl variant="outlined" >
+                                <InputLabel htmlFor="outlined-sort-simple"> {t("Categories")} </InputLabel>
+                                <Select type="text" name="newCategory" value={this.state.name} onChange={this.update} labelWidth={70} inputProps={{ name: 'categories', id: 'outlined-status-simple', }} >
+                                    {this.state.categories.map(c =>
+                                        <MenuItem value={c}>{c}</MenuItem>)}
+                                </Select>
+                            </FormControl>
 
-                    <Typography color="textSecondary" gutterBottom> {t("Add New Status")}  </Typography>
-                    <div id="content">
-                        <TextField style={{ width: "35%" }} id="outlined-name" label={t("New Status")} name="statusPost" value={this.state.name} onChange={this.update} margin="normal" variant="outlined" type="text" />
-                    </div>
-
-                    <FormControl variant="outlined" >
-                        <InputLabel htmlFor="outlined-sort-simple"> {t("Status")} </InputLabel>
-                        <Select type="text" name="statusPost" value={this.state.name} onChange={this.update} labelWidth={70} inputProps={{ name: 'statusPost', id: 'outlined-status-simple', }} >
-                            {this.state.statusPost.map(s =>
-                            <MenuItem value={s}>{s}</MenuItem>)}
-                        </Select>
-                    </FormControl>
-
-                </div>
-                {this.state.content && this.state.employee && this.state.status ?
-                    <Button variant="outlined" type="submit" onClick={this.sendResp}> {("Send")} </Button> :
-                    <Button disabled variant="outlined" type="submit" onClick={this.sendResp}> {t("Send")} </Button>
-                }
-
+                            <Typography color="textPrimary" gutterBottom> {t("Add New Category")}  </Typography>
+                            <div id="content">
+                                <TextField style={{ width: "70%" }} id="outlined-name" label={t("New Category")} name="newCategory" value={this.state.name} onChange={this.update} margin="normal" variant="outlined" type="text" />
+                            </div>
+                        </div>
+                        {this.state.newCategory ?
+                            <Button variant="outlined" type="submit" onClick={this.addCategory}> {("Send")} </Button> :
+                            <Button disabled variant="outlined" type="submit" onClick={this.addCategory}> {"Send"} </Button>
+                        }
+                    </CardContent>
+                </Card>
             </div>
         )
     }
