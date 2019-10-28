b17c1a366d4ce3431201991a601d1a1f0cee9108
pagella42 <pagella42@gmail.com>
2019-10-28 15:57:08 -0500

    landing

--- src/user/landing/LandingPage.js
+++ src/user/landing/LandingPage.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import './LandingPage.css'
+import Fab from '@material-ui/core/Fab';
+
+
+
 class LandingPage extends Component {
     render() {
         return (
             <div className="landing-page">
-		<div>Comunidad Pedasí</div>
+                <div className="landinginner">
+
+		<h1 style={{fontSize: "4em", fontWeight: "lighter"}}>Comunidad Pedasí</h1>
+      <Link to="/user/home" style={{textDecoration: 'none',}}>  <Fab color="primary" id="home" variant="extended" aria-label="language" size="large" onClick={this.changeLanguage}>Go to Home</Fab></Link>
+                </div>
+
 
 		
             </div>
